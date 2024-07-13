a96611fe9a93bad455d967b1c2845950365836dd
JC MISA <johncarlomisa399@gmail.com>
2024-07-13 16:29:06 +0800

    change the bento grid globe title

--- data/index.ts
+++ data/index.ts
@@ -22,7 +22,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "Proven flexibility approach to project needs",
+    title: "Proven flexibility approach",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
